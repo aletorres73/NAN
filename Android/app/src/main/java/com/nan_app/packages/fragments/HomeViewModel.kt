7a6b388739fb99643fc23f6ef22a2d5e519774bb
aletorres73 <torresalejandro7392@gmail.com>
2024-01-23 22:03:10 -0300

    repo local actualizado

---
+++ Android/app/src/main/java/com/nan_app/packages/fragments/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.nan_app.packages.fragments
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- Python/webScraping/webScrapingClienst.py
+++ Python/webScraping/webScrapingClienst.py
@@ -1,36 +1,40 @@
-import requests
+# import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+# from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 
 
 
 def buscar_vendedores_calzado(zona):
 
-    options = Options()
-    options.headless = True  # Para ejecutar Firefox en modo headless, sin abrir una ventana visual
+    # options = Options()
+    # options.headless = True  # Para ejecut|ar Firefox en modo headless, sin abrir una ventana visual
 
-    binary = FirefoxBinary('/usr/bin/firefox')
-    driver = webdriver.Firefox(firefox_binary=binary, options=options)
+    # binary = FirefoxBinary('/usr/bin/firefox')
+    # binary = FirefoxBinary("C:\Program Files\Mozilla Firefox")
+    # driver = webdriver.Firefox(options=options, service=binary)
+    
+    options = FirefoxOptions()
+    options.add_argument('--headless')
+    # options.set_preference("browser.download.manager.showWhenStarting", False)
+    options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+    driver = webdriver.Firefox(options = options)
 
     url = f"https://www.google.com/search?q=vendedores+de+calzado+{zona}"
 
     driver.get(url)
 
-    element = driver.find_elements('css selector','yuRUbf')
-
-    html = element.get_attribute("outerHTML")
+    element = driver.find_elements(By.CSS_SELECTOR,'div.yuRUbf h3.LC20lb')
+    
+    html = '\n'.join([h_element.get_attribute("innerHTML") for h_element in element])
 
     driver.quit()
 
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-    
     soup = BeautifulSoup(html, "html.parser")
 
     # resultados = soup.select(".tF2Cxc")  # Selector para los resultados de búsqueda
@@ -45,16 +49,6 @@ def buscar_vendedores_calzado(zona):
         vendedor = {"Nombre": titulo, "Zona": zona, "Teléfono": "", "Enlace": enlace}
         vendedores.append(vendedor)
 
-    # # Iterar por los vendedores y obtener información adicional
-    # for vendedor in vendedores:
-    #     response = requests.get(vendedor["Enlace"], headers=headers)
-    #     soup = BeautifulSoup(response.text, "html.parser")
-
-    #     # Agregar código para obtener el número de teléfono o información adicional del vendedor
-    #     # Utiliza técnicas de web scraping similares para extraer la información deseada
-
-    #     # Actualiza el diccionario del vendedor con la información adicional
-    #     vendedor["Teléfono"] = "123456789"  # Ejemplo: Asigna un número de teléfono ficticio
 
     # Crear DataFrame con los resultados
     df = pd.DataFrame(vendedores)
