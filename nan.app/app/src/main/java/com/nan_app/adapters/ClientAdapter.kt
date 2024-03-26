31ce48ee27919d4eff254af0d3d82dd7c4c28f28
aletorres73 <torresalejandro7392@gmail.com>
2024-03-26 11:32:12 -0300

    Se ajustan las vistas de la lista del home, se crean estilos para los textos

--- nan.app/app/src/main/java/com/nan_app/adapters/ClientAdapter.kt
+++ nan.app/app/src/main/java/com/nan_app/adapters/ClientAdapter.kt
@@ -11,42 +11,53 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.nan_app.R
+import com.nan_app.databinding.ItemClientBinding
 import com.nan_app.entities.Clients
 
 class ClientAdapter(
     private var clientlist: MutableList<Clients>,
     private val clickListener: ClientClickListener
-): RecyclerView.Adapter<ClientAdapter.ClientHolder>() {
+) : RecyclerView.Adapter<ClientAdapter.ClientHolder>() {
 
     class ClientHolder(v: View) : RecyclerView.ViewHolder(v) {
         private var view: View
-        init { this.view = v }
+
+//        private var binding = ItemClientBinding.bind(v)
+        init {
+            this.view = v
+        }
+
         private fun setName(name: String) {
             val txtName: TextView = view.findViewById(R.id.txtName)
-            txtName.text = name }
+            txtName.text = name
+        }
+
         private fun setLastName(lastname: String) {
             val txtLastName: TextView = view.findViewById(R.id.txtLastName)
-            txtLastName.text = lastname }
-        private fun setId(id : String){
-            val txtId : TextView = view.findViewById(R.id.txtId)
-            txtId.text = id }
-        private fun setImageClient(uri : String){
-            val imageClient : ImageView =view.findViewById(R.id.imageProductItem)
-            if(uri != "null")
-                if (uri != "")
-                {
+            txtLastName.text = lastname
+        }
+
+        private fun setId(id: String) {
+            val txtId: TextView = view.findViewById(R.id.txtId)
+            txtId.text = id
+        }
+
+        private fun setImageClient(uri: String) {
+            val imageClient: ImageView = view.findViewById(R.id.imageProductItem)
+            if (uri != "null")
+                if (uri != "") {
                     Glide.with(imageClient.context)
-                    .load(uri)
-                    .into(imageClient)
+                        .load(uri)
+                        .into(imageClient)
                 }
         }
+
         @SuppressLint("SetTextI18n")
-        fun setBirthday(date: String){
+        fun setBirthday(date: String) {
             val dateBirthday: TextView = view.findViewById(R.id.textBirthday)
-            if(date != "")
-            {
+            if (date != "") {
                 val birthday = date.split("/").toMutableList()
-                when (birthday[1]){
+                when (birthday[1]) {
                     '1'.toString() -> birthday[1] = "Enero"
                     '2'.toString() -> birthday[1] = "Febrero"
                     '3'.toString() -> birthday[1] = "Marzo"
@@ -56,39 +67,46 @@ class ClientAdapter(
                     '7'.toString() -> birthday[1] = "Julio"
                     '8'.toString() -> birthday[1] = "Agosto"
                     '9'.toString() -> birthday[1] = "Septiembre"
-                    "10"-> birthday[1] = "Octubre"
-                    "11"-> birthday[1] = "Noviembre"
-                    "12"-> birthday[1] = "Diciembre" }
+                    "10" -> birthday[1] = "Octubre"
+                    "11" -> birthday[1] = "Noviembre"
+                    "12" -> birthday[1] = "Diciembre"
+                }
                 dateBirthday.text = "${birthday[0]} de ${birthday[1]}"
-            }
-            else
+            } else
                 dateBirthday.text = date
         }
 
-        private fun setPayDay(date: String){
+        private fun setPayDay(date: String) {
             val datePayDay: TextView = view.findViewById(R.id.txtPayDay)
-            datePayDay.text = date }
+            datePayDay.text = date
+        }
 
-        private fun setFinishDay(date: String){
+        private fun setFinishDay(date: String) {
             val dateFinishDay: TextView = view.findViewById(R.id.txtFinishDay)
-            dateFinishDay.text = date }
+            dateFinishDay.text = date
+        }
 
         fun getCard(): CardView {
-            return view.findViewById(R.id.cardImage)}
+            return view.findViewById(R.id.cardImage)
+        }
 
-        fun getButtonDelete() : Button{
-            return view.findViewById(R.id.btnDeleteClient)}
-        fun getButtonEdit():Button{
-            return view.findViewById(R.id.btnEditClient)}
+        fun getButtonDelete(): Button {
+            return view.findViewById(R.id.btnDeleteClient)
+        }
 
-        fun bind(client: Clients){
+        fun getButtonEdit(): Button {
+            return view.findViewById(R.id.btnEditClient)
+        }
+
+        fun bind(client: Clients) {
             setName(client.Name)
             setLastName(client.LastName)
             setId(client.id.toString())
             setImageClient(client.ImageUri)
             setBirthday(client.Birthday)
             setPayDay(client.PayDay)
-            setFinishDay(client.FinishDay) }
+            setFinishDay(client.FinishDay)
+        }
     }
 
     override fun getItemCount() = clientlist.size
@@ -112,7 +130,7 @@ class ClientAdapter(
         }
     }
 
-    fun updateList(listClient: MutableList<Clients>){
+    fun updateList(listClient: MutableList<Clients>) {
         this.clientlist = listClient
         notifyDataSetChanged()
     }
